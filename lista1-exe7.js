24e4861e856e9f24bd4ad0d817a070617edfd1bb
DAniel Pires <dfpires@gmail.com>
2020-08-26 21:34:44 -0300

    exercício 7 pronto mas sem o cálculo da média

--- lista1-exe7.js
+++ lista1-exe7.js
@@ -1,3 +1,21 @@
 
+chefe()
 
-console.log(`Exercício 7`)
+function chefe(){
+    var vetor = [] // declara o vetor
+    exe7(vetor) // estamos passando o vetor como parâmetro - escopo global
+
+}
+
+function exe7(vet){ // vet representa vetor -> o que fizermos com vet, será feito em vetor
+
+    var numero = Number(prompt(`Informe um numero`))
+    do {
+
+        // coloca o numero em vet
+        vet.push(numero)
+        numero = Number(prompt(`Informe outro numero, informe número negativo para encerrar`))
+    }
+    while (numero >= 0)
+}
+
